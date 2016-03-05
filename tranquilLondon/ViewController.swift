281cf4ca217db60f1ac815301103c2873d04fdcc
Daniel Pape <danielpape@daniels-mbp-2.home>
2016-03-05 08:51:00 -0800

    Initial Commit

---
+++ tranquilLondon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tranquilLondon
+//
+//  Created by Daniel Pape on 05/03/2016.
+//  Copyright © 2016 Daniel Pape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
